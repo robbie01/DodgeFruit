04e5ea900689c13215c8251071c87c8b9df25c71
Robbie Langer <robbie@sohio.net>
2021-12-10 02:26:53 -0500

    prevent negative oob access (caught by openbsd)

--- src/LCD.cpp
+++ src/LCD.cpp
@@ -130,6 +130,8 @@ void LCDClass::SetBackgroundColor(pixel_t bgcolor) {
 }
 
 void LCDClass::DrawPixel(int x, int y) {
+    while (x < 0) x += SCREEN_SIZE_X;
+    while (y < 0) y += SCREEN_SIZE_Y;
     ((pixel_t*)((char*)sur->pixels+(y % SCREEN_SIZE_Y)*sur->pitch))[x % SCREEN_SIZE_X] = color;
 }
 
