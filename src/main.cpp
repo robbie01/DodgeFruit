95592721bab8a0246ec76ebda716953db102dbb9
robbie <robbie@sohio.net>
2022-01-25 15:00:15 -0500

    reuse loop code between emscripten and native

--- src/main.cpp
+++ src/main.cpp
@@ -17,18 +17,29 @@ void writeTime(float);
 float playerHighScore();
 int gameCount();
 
-#ifdef __EMSCRIPTEN__
-void em_loop_callback(void *data) {
+#ifndef __EMSCRIPTEN__
+inline bool
+#else
+void
+#endif
+loop_inner(void *data) {
     ScreenPtr &cur_screen = *(ScreenPtr*)data;
     
     auto ret = cur_screen->update();
-    if (ret == ScreenUpdateReturn(std::monostate())) throw std::exception();
+    if (ret == ScreenUpdateReturn(std::monostate()))
+#ifndef __EMSCRIPTEN__
+        return false;
+#else
+        throw std::exception();
+#endif
     else if (std::get<1>(ret) != nullptr) {
         cur_screen = std::move(std::get<1>(ret));
         cur_screen->init();
     }
-}
+#ifndef __EMSCRIPTEN__
+    return true;
 #endif
+}
 
 /*
  * Entry point to the application
@@ -43,17 +54,10 @@ int main(int argc, char *argv[]) {
     ScreenPtr cur_screen(new MenuScreen);
     cur_screen->init();
 
-#ifdef __EMSCRIPTEN__
-    emscripten_set_main_loop_arg(em_loop_callback, &cur_screen, 0, true);
+#ifndef __EMSCRIPTEN__
+    while (loop_inner(&cur_screen));
 #else
-    while (true) {
-        auto ret = cur_screen->update();
-        if (ret == ScreenUpdateReturn(std::monostate())) break;
-        else if (std::get<1>(ret) != nullptr) {
-            cur_screen = std::move(std::get<1>(ret));
-            cur_screen->init();
-        }
-    }
+    emscripten_set_main_loop_arg(loop_inner, &cur_screen, 0, true);
 #endif
     SDL_Quit();
     return 0;
